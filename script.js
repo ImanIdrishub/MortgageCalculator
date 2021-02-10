0239614354092ebe203d0f1985c6ec9bd6275838
“Iman” <i.idris.iman@gmail.com>
2021-02-09 21:27:51 -0500

    formula donee

--- script.js
+++ script.js
@@ -1,3 +1,15 @@
 function myFunction() {
-    document.getElementById("outmonthly").innerHTML = "Hello World";
+    const mortgageAmount = document.getElementById("Amount").value
+    const interestRate = document.getElementById("interestrate").value
+    const payment = monthlyPayment(Number(mortgageAmount), Number(interestRate))
+    document.getElementById("monthlymortgagepayment").innerText = `Total Monthly Payment: ${payment}`
   }
+
+
+function monthlyPayment(p, interestRate) {
+   // console.log(typeof p) //prints to the console
+    const n = 25*12 
+    const i = interestRate/12/100
+    let x = p * i * (Math.pow(1 + i, n)) / (Math.pow(1 + i, n) - 1)
+  return x.toFixed(2);
+}
